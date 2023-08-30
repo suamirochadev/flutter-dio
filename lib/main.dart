f60b0ec946c32e915c4dd5727c6e8466dddefb8c
Suami Rocha <suamirochati@gmail.com>
2023-08-30 11:19:28 -0300

    Calculadora IMC

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,136 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class Home extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  String _info = "Informe seus dados.";
+
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  TextEditingController pesoController = TextEditingController();
+  TextEditingController alturaController = TextEditingController();
+
+  void _resetFields()
+  {
+      pesoController.text = '';
+      alturaController.text = '';
+      setState(() {
+        _info = "Informe seus dados.";
+        _formKey = GlobalKey<FormState>();
+      });
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  void _calcular(){
+    setState(()
+    {
+      double peso = double.parse(pesoController.text);
+      double altura = double.parse(alturaController.text) / 100;
+      double imc = peso / ( altura * altura);
+      print(imc);
+      if(imc < 18.6){
+        _info = 'Abaixo do Peso (${imc.toStringAsPrecision(3)})';
+      } else if(imc >= 18.6 && imc < 24.9){
+        _info = 'Peso Ideal (${imc.toStringAsPrecision(3)})';
+      } else if(imc >= 24.9 && imc < 29.9){
+        _info = 'Levemente Acima do Peso (${imc.toStringAsPrecision(3)})';
+      } else if(imc >= 29.9 && imc < 34.9){
+        _info = 'Obesidade Grau I (${imc.toStringAsPrecision(3)})';
+      } else if(imc >= 34.9 && imc < 39.9){
+        _info = 'Obesidade Grau II (${imc.toStringAsPrecision(3)})';
+      } else if(imc >= 40){
+        _info = 'Obesidade Grau III (${imc.toStringAsPrecision(3)})';
+      }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Calculadora IMC"),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 147, 76, 175),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.refresh), onPressed: _resetFields)
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(Icons.person_outline, size: 120.0, color: const Color.fromARGB(255, 172, 76, 175)),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                    labelText: "Peso (kg)",
+                    labelStyle: TextStyle(color: Color.fromARGB(255, 167, 76, 175))),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: const Color.fromARGB(255, 158, 76, 175), fontSize: 25.0),
+                controller: pesoController,
+                validator: (value){
+                  if(value!.isEmpty){
+                    return "Insira seu Peso!";
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Altura (CM)",
+                    labelStyle: TextStyle(color: const Color.fromARGB(255, 163, 76, 175))),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: const Color.fromARGB(255, 175, 76, 170), fontSize: 25.0),
+                controller: alturaController,
+                validator: (value){
+                  if(value!.isEmpty){
+                    return "Insira sua Altura!";
+                  }
+                  return null;
+                },
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                child: Container(
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: (){
+                      if(_formKey.currentState!.validate()){
+                        _calcular();
+                      }
+                    },
+                    child: Text('Calcular',
+                      style: TextStyle(color: Colors.white, fontSize: 25.0),),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor:Color.fromARGB(255, 133, 31, 124),
+                    )
+                  ),
+                ),
+              ),
+              Text(_info,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: const Color.fromARGB(255, 139, 76, 175), fontSize: 25),
+              )
+            ],
+          ),
+        )
+      )
     );
   }
 }
